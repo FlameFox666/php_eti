69901bb237e6d03f629ded4fc7126422640ab38a
FlameFox666 <lexarole@gmail.com>
2024-05-26 11:06:22 +0300

    finally

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('home') }}">Home</a>
+        </li>
+
+        <li>
+            <a href="{{ route('contacts') }}">Contacts</a>
+        </li>
+
+        <li>
+            <a href="{{ route('about') }}">About</a>
+        </li>
+    </ul>
+</nav>
